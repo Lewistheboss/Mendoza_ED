ac5237096e0b55d3e0f172f0e719ddbbebc6c6c9
Lewistheboss <lumenri.lm@gmail.com>
2025-06-17 17:02:50 +0200

    Segunda clase Rectangulo Luis

--- src/Mendoza_circulo.java
+++ src/Mendoza_circulo.java
@@ -1,4 +1,62 @@
 
 public class Mendoza_circulo {
 
+	private int id;
+    private String nombre;
+    private double radio;
+    private String color;
+    //Constructor por defecto
+    public Mendoza_circulo() {
+		super();
+	}
+    //Constructor con argumentos 
+    public Mendoza_circulo(int id, String nombre, double radio, String color) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.radio = radio;
+		this.color = color;
+	}
+//Constructor con argumentos sin id 
+	public Mendoza_circulo(String nombre, double radio, String color) {
+		super();
+		this.nombre = nombre;
+		this.radio = radio;
+		this.color = color;
+	}
+	
+    //Getter y Setter
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getRadio() {
+		return radio;
+	}
+	public void setRadio(double radio) {
+		this.radio = radio;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+    
+	//ToString
+	
+	@Override
+	public String toString() {
+		return "Mendoza_circulo [id=" + id + ", nombre=" + nombre + ", radio=" + radio + ", color=" + color + "]";
+	}
 }
---
+++ src/Mendoza_rectangulo.java
@@ -0,0 +1,70 @@
+
+public class Mendoza_rectangulo {
+//Atributos 
+	 private int id;
+	    private double ladoA;
+	    private double ladoB;
+	    private String color;
+	
+//Cosntructor por defecto
+		public Mendoza_rectangulo() {
+			super();
+		}  
+//Constructor sin id
+
+public Mendoza_rectangulo(double ladoA, double ladoB, String color) {
+	super();
+	this.ladoA = ladoA;
+	this.ladoB = ladoB;
+	this.color = color;
+}
+//constructor generico
+public Mendoza_rectangulo(int id, double ladoA, double ladoB, String color) {
+	super();
+	this.id = id;
+	this.ladoA = ladoA;
+	this.ladoB = ladoB;
+	this.color = color;
+}
+// getter y setter
+public int getId() {
+	return id;
+}
+
+public void setId(int id) {
+	this.id = id;
+}
+
+public double getLadoA() {
+	return ladoA;
+}
+
+public void setLadoA(double ladoA) {
+	this.ladoA = ladoA;
+}
+
+public double getLadoB() {
+	return ladoB;
+}
+
+public void setLadoB(double ladoB) {
+	this.ladoB = ladoB;
+}
+
+public String getColor() {
+	return color;
+}
+
+public void setColor(String color) {
+	this.color = color;
+}
+//ToString
+@Override
+public String toString() {
+	return "Mendoza_rectangulo [id=" + id + ", ladoA=" + ladoA + ", ladoB=" + ladoB + ", color=" + color + "]";
+}
+
+
+
+		
+}
